fe81aff4f6411f98a84ac8d5fa391169859d7de2
Jason Kraus <zbyte64@gmail.com>
2016-02-01 12:46:20 -0800

    allow pem files to be passed as environment variables

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ You must provide the volume /certs with the following files:
 * server-key.pem
 * ca.pem
 
+Or provide the pem files through environment variables:
+
+* SERVER_CERT
+* SERVER_KEY
+* CA_CERT
+
 Client connections will be verified with ca.pem
--- resources/run.sh
+++ resources/run.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-#TODO exit if either fails/stop
+if [ $CA_CERT ]
+then
+  echo $CA_CERT > /cert/ca.pem
+fi
+
+if [ $SERVER_CERT ]
+then
+  echo $SERVER_CERT > /cert/server.pem
+fi
+
+if [ $SERVER_KEY ]
+then
+  echo $SERVER_KEY > /cert/server-key.pem
+fi
+
 
 stunnel /opt/stunnel.conf & \
 rethinkdb proxy --join $RETHINKDB_JOIN_ADDR --driver-port 28015
