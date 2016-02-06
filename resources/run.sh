31ee684c98c402bd37ddf7da9ba4e069795fafa8
Jason Kraus <zbyte64@gmail.com>
2016-02-05 17:37:20 -0800

    log ca

--- resources/run.sh
+++ resources/run.sh
@@ -15,6 +15,8 @@ then
   echo $SERVER_KEY > /certs/server-key.pem
 fi
 
+echo "Authenticating Certificate Authority:"
+cat /certs/ca.pem
 
 stunnel /opt/stunnel.conf & \
 rethinkdb proxy --join $RETHINKDB_JOIN_ADDR --driver-port 28015
